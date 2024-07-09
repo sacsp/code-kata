3aae2ed2ed76b1f07ee2e7f80f04352d02a56143
Sachin patil <sachin@Sachins-Air.lan>
2024-07-09 21:26:44 +1000

    second commit

--- p1-run.sh
+++ p1-run.sh
@@ -11,14 +11,14 @@ echo "RUNNING JOB"
 # executor_memory=10g
 
 # Set the path to the spec file and the input file
-SPEC_FILE="Input_files/spec.json"
-INPUT_FILE="Input_files/input.txt"
+SPEC_FILE="/app/Input_files/spec.json"
+INPUT_FILE="/app/Input_files/input.txt"
 
 # PROJECT: Run the main Python script
 echo ""
 echo "$(date)"
 echo "====================================="
 echo "Running main.py script == Started"
-source ./venv/bin/activate && python problem-1/main.py "$SPEC_FILE" "$INPUT_FILE"
+source ./venv/bin/activate && python3 problem-1/main.py "$SPEC_FILE" "$INPUT_FILE"
 echo "Running main.py script == Completed"
 echo "**************************************"
