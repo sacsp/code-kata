494c599c0f7b5ddcb3cf3ab64f93ac1feb07b7d6
Sachin patil <sachin@Sachins-Air.lan>
2024-07-10 02:06:23 +1000

    fix in run.sh

--- p2-run.sh
+++ p2-run.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
-
-# Source the profile
-source /etc/profile
-
-# Set environment variables
-export PYSPARK_PYTHON=python3
+# Activate the virtual environment
+source ./venv/bin/activate
 
 echo "RUNNING JOB"
 driver_memory=10g
 executor_memory=10g
 
-# PROJECT: Run the main Python script
-echo ""
-echo "$(date)"
-echo "====================================="
-echo "Running pysaprk script for csv to anonymize == Started"
-source ./venv/bin/activate && ./venv/lib/python3.9/site-packages/pyspark/bin/spark-submit --driver-memory ${driver_memory} --executor-memory ${executor_memory} problem-2/generate_csv_spark.py
-source ./venv/bin/activate && ./venv/lib/python3.9/site-packages/pyspark/bin/spark-submit --driver-memory ${driver_memory} --executor-memory ${executor_memory} problem-2/anonymize_spark.py
-echo "Running pysaprk script for csv to anonymize == Completed"
-echo "*************************************"
+# Run the first command
+./venv/lib/python3.9/site-packages/pyspark/bin/spark-submit --driver-memory ${driver_memory} --executor-memory ${executor_memory} problem-2/generate_csv_spark.py
+
+# Run the second command (example)
+./venv/lib/python3.9/site-packages/pyspark/bin/spark-submit --driver-memory ${driver_memory} --executor-memory ${executor_memory} problem-2/anonymize_spark.py
+
+# Add more commands as needed
+
+
+
+#
+##!/bin/bash
+#
+## Source the profile
+#source /etc/profile
+#
+## Set environment variables
+#export PYSPARK_PYTHON=python3
+#
+#echo "RUNNING JOB"
+#driver_memory=10g
+#executor_memory=10g
+#
+## PROJECT: Run the main Python script
+#echo ""
+#echo "$(date)"
+#echo "====================================="
+#echo "Running pysaprk script for csv to anonymize == Started"
+#source ./venv/bin/activate && ./venv/lib/python3.9/site-packages/pyspark/bin/spark-submit --driver-memory ${driver_memory} --executor-memory ${executor_memory} problem-2/generate_csv_spark.py
+#source ./venv/bin/activate && ./venv/lib/python3.9/site-packages/pyspark/bin/spark-submit --driver-memory ${driver_memory} --executor-memory ${executor_memory} problem-2/anonymize_spark.py
+#echo "Running pysaprk script for csv to anonymize == Completed"
+#echo "*************************************"
